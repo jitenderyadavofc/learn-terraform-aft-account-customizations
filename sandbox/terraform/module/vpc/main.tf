44974ba9d7681dd08958e167bfa33d72a5f1e628
jitenderyadavofc <157213132+jitenderyadavofc@users.noreply.github.com>
2024-02-07 22:18:55 +0530

    Update main.tf

--- sandbox/terraform/module/vpc/main.tf
+++ sandbox/terraform/module/vpc/main.tf
@@ -12,3 +12,19 @@ resource "aws_vpc" "aws-vpc" {
    
 }
 
+data "aws_vpc_ipam_pool" "test" {
+  filter {
+    name   = "description"
+    values = ["*app1*"]
+  }
+ 
+  filter {
+    name   = "address-family"
+    values = ["ipv4"]
+  }
+}
+ 
+resource "aws_vpc" "test" {
+  ipv4_ipam_pool_id   = data.aws_vpc_ipam_pool.test.id
+  ipv4_netmask_length = 18
+}
