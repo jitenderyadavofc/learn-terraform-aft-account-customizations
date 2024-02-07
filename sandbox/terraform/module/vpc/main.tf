41384cc5460720116cbdf1e7e463ca879dfcdd25
jitenderyadavofc <157213132+jitenderyadavofc@users.noreply.github.com>
2024-02-07 22:22:17 +0530

    Update main.tf

--- sandbox/terraform/module/vpc/main.tf
+++ sandbox/terraform/module/vpc/main.tf
@@ -1,17 +1,3 @@
-resource "aws_vpc" "aws-vpc" {
-     cidr_block = var.cidr_block
-  
-    tags = merge( 
-     {"Name"=var.name},
-    {
-     
-     for key1,value1 in var.tag_names:  
-        key1=>value1
-        
-        })
-   
-}
-
 data "aws_vpc_ipam_pool" "test" {
   filter {
     name   = "description"
@@ -24,7 +10,18 @@ data "aws_vpc_ipam_pool" "test" {
   }
 }
  
-resource "aws_vpc" "test" {
+
+resource "aws_vpc" "aws-vpc" {
   ipv4_ipam_pool_id   = data.aws_vpc_ipam_pool.test.id
   ipv4_netmask_length = 18
+
+
+tags = merge( 
+     {"Name"=var.name},
+    {
+     
+     for key1,value1 in var.tag_names:  
+        key1=>value1
+        
+        })
 }
